2a2cf6d7f1e88042f0e6f4dafa310393c62444af
licht1stein <mb@blaster.ai>
2021-10-11 13:58:09 +0300

    slight update in logging

--- main.py
+++ main.py
@@ -18,30 +18,35 @@ ALERT_CHANNEL = env('ALERT_CHANNEL')
 
 bot = Bot(BOT_TOKEN)
 
-laws_file = Path('../laws.json')
+laws_file = Path('laws.json')
 laws = json.load(laws_file.open('r'))
 
 
 def check_law(law):
-    print(f'Checking law: {law["name"]}')
+    print(f'Проверка обновлений: {law["name"]}')
     data = requests.get(law['url'])
     soup = BeautifulSoup(data.text, 'html5lib')
 
     pattern = re.compile(r'\d\d.\d\d.\d\d\d\d')
     dates = [dt.from_format(d, 'DD.MM.YYYY').date() for d in pattern.findall(soup.text)]
-    print(f"Last update: {max(dates).format('DD.MM.YYYY')}")
-    new_dates = [d for d in dates if d > dt.parse(law['updated']).date()]
-    formatted_dates = [d.format('DD.MM.YYYY') for d in new_dates]
-    for d in formatted_dates:
+
+    expected_updated = dt.parse(law['updated']).date()
+    actual_updated = max(dates)
+    print(f'Ожидаемое последнее обновление: {expected_updated.format("DD.MM.YYYY")}')
+    print(f"Фактическое последнее обновление: {actual_updated.format('DD.MM.YYYY')}")
+
+    if expected_updated < actual_updated:
         bot.send_message(ALERT_CHANNEL, f'Найдено обновление закона!\n\n<a href="{law["url"]}">{law["name"]}'
-                                        f'</a> обновлен {d}', parse_mode='HTML')
+                                        f'</a> обновлен {actual_updated.format("DD.MM.YYYY")}', parse_mode='HTML')
+    else:
+        print(f"Без изменений: {law['name']}")
 
 
 if __name__ == '__main__':
-    print("Starting rada-tracker")
+    print("rada-tracker: запуск")
     for l in laws:
         check_law(l)
-    print("Sleeping 12 hours")
+    print("Ждем 12 часов")
     time.sleep(12 * 60 * 60)
 
 
